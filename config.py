4e67c145121f5dd1f2557a9404cb25dab566e0cd
Matt Webb <matt@interconnected.org>
2014-09-27 13:24:41 +0100

    preparing to do a test deploy to heroku

--- config.py
+++ config.py
@@ -3,20 +3,49 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'this is the lp2 secret'
-    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-
-    @staticmethod
-    def init_app(app):
-        pass
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'this is the lp2 secret'
+	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+	
+	@staticmethod
+	def init_app(app):
+		pass
 
 class DevelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class ProductionConfig(Config):
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+	
+	@classmethod
+	def init_app(cls, app):
+		Config.init_app(app)
+	
+class HerokuConfig(ProductionConfig):
+	SSL_DISABLE = bool(os.environ.get('SSL_DISABLE'))
+	
+	@classmethod
+	def init_app(cls, app):
+		ProductionConfig.init_app(app)
+
+		# handle proxy server headers
+		from werkzeug.contrib.fixers import ProxyFix
+		app.wsgi_app = ProxyFix(app.wsgi_app)
+		
+		# log to stderr
+		import logging
+		from logging import StreamHandler
+		file_handler = StreamHandler()
+		file_handler.setLevel(logging.WARNING)
+		app.logger.addHandler(file_handler)
+
 
 config = {
-    'development': DevelopmentConfig,
-    
-    'default': DevelopmentConfig
+	'development': DevelopmentConfig,
+	'production': ProductionConfig,
+	'heroku': HerokuConfig,
+	
+	'default': DevelopmentConfig
 }
