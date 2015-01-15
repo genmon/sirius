2498519232a7215039ecfcd15a37c699137fa27c
Tom Hunger <tehunger@gmail.com>
2015-01-15 16:44:05 +0000

    Remove previous version of sirius. It's been a good guide, so long.

--- app/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# the top-level models module needs to import all models so that the
-# database migration tool can detect them
-from .core import BridgeCommand, DeviceCommand, Bridge, Device, PendingClaim
