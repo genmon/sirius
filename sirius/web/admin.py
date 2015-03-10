ddd0920f4baee16dac746c1ab7e3bbfcb9a63458
Tom Hunger <tehunger@gmail.com>
2015-03-10 10:45:23 +0000

    Remove testing code for set_quip and adjust set_personality.

--- sirius/web/admin.py
+++ sirius/web/admin.py
@@ -29,34 +29,9 @@ def randomly_change_personality():
     msg = messages.SetPersonality(
         device_address='000d6f000273ce0b',
         face_pixels=im,
-        nothing_to_print_pixels=im,
-        cannot_see_bridge_pixels=im,
-        cannot_see_internet_pixels=im,
-    )
-    success, next_print_id = protocol_loop.send_message(
-        '000d6f000273ce0b', msg)
-
-    if success:
-        flask.flash('Sent your message to the printer!')
-    else:
-        flask.flash(("Could not send message because the "
-                     "printer {} is offline.").format('000d6f000273ce0b'),
-                    'error')
-
-    return flask.redirect('/admin')
-
-
-@blueprint.route('/admin/randomly-change-quip', methods=['GET', 'POST'])
-def randomly_change_quip():
-    # Restrict to debug for now until we have a proper user management
-    # system.
-    assert flask.current_app.config['DEBUG'], "Debug not enabled"
-
-    msg = messages.SetQuip(
-        device_address='000d6f000273ce0b',
-        quip_pixels_1=image_encoding.default_pipeline('quip 1'),
-        quip_pixels_2=image_encoding.default_pipeline('quip 2'),
-        quip_pixels_3=image_encoding.default_pipeline('quip 3'),
+        nothing_to_print_pixels=image_encoding.default_pipeline('Nothing to print'),
+        cannot_see_bridge_pixels=image_encoding.default_pipeline('Cannot see bridge'),
+        cannot_see_internet_pixels=image_encoding.default_pipeline('Cannot see internet'),
     )
     success, next_print_id = protocol_loop.send_message(
         '000d6f000273ce0b', msg)
